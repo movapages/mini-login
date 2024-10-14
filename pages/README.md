9d3631f55154f4c05f7f5a0546300bdde3b2caa5
Oleksandr Mostovyi <om@mova.club>
2024-10-14 17:08:44 +0300

    Demo Apps

---
+++ pages/README.md
@@ -0,0 +1,9 @@
+#### Next+React Web App
+
+>This is a tiny demo application using `next-auth` and a couple of react components.
+
+##### Web App Demo
+
+[App demo](../mini-login-video.gif)
+
+
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, {Session} from "next-auth";
 import CredentialsProvider, {CredentialsProviderType} from "next-auth/providers/credentials";
-import { compare } from "bcrypt-ts";
-import { client } from "../../auth/sanity-client";
+// import { compare } from "bcrypt-ts";
+// import { client } from "../../auth/sanity-client";
 
 export default NextAuth({
   providers: [
@@ -18,44 +18,35 @@ export default NextAuth({
 
         // Query Sanity for the user with the given email
         // const query = `*[_type == "UserAccount" && lower(Username) == lower($email)][0] {
-        const query = `*[_type == "UserAccount" && lower(Username) == "admin1@example.com"][0] {
-          "id": UserId,
-          Username,
-          PasswordHash,
-          UserRole-> {
-            RoleName,
-            RolePermissions
-          }
-        }`;
+        //   "id": UserId,
+        //   Username,
+        //   PasswordHash,
+        //   UserRole-> {
+        //     RoleName,
+        //     RolePermissions
+        //   }
+        // }`;
 
-        // console.log("DB-query: ", query);
-        // console.log("credentials.email: ", credentials.email);
-        // console.log("credentials.password: ", credentials.password);
-
-        let user = null;
-        try {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-expect-error
-          user = await client.fetch(query, {email: credentials.email});
-        } catch (error) {
-          console.error('Error fetching from Sanity:', error);
-        }
-
-        // console.log("DB-CLIENT: ", client);
-        console.log("DB-user: ", user);
+        // let user = null;
+        // try {
+          // user = await client.fetch(query, {email: credentials.email});
+        //   user = await client.fetch(query, {email: "admin1@example.com"});
+        // } catch (error) {
+        //   console.error('Error fetching from Sanity:', error);
+        // }
 
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        if (user && await compare(credentials.password, user.PasswordHash)) {
-          return {
-            id: user.id,
-            email: user.Username,
-            role: user.UserRole.Rolename,
-            permissions: user.UserRole.RolePermissons,
-          };
-        } else {
-          return null;
-        }
+        // --@ts-expect-error
+        // if (user && await compare(credentials.password, user.PasswordHash)) {
+        //   return {
+        //     id: user.id,
+        //     email: user.Username,
+        //     role: user.UserRole.Rolename,
+        //     permissions: user.UserRole.RolePermissons,
+        //   };
+        // } else {
+        //   return null;
+        // }
 
         // id:1001
         // Username:admin1@example.com
@@ -63,32 +54,21 @@ export default NextAuth({
         // UserRole:{…} 2 properties
         // RoleName:Admin
         // RolePermissions:DeleteEvent
-        // const users = [
-        //   { id: "1001", email: "admin1@example.com", password: "admin123" },
-        //   { id: "1002", email: "user1@example.com", password: "user123" }
-        // ];
-        // const user = users.find(
-        //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //   // @ts-expect-error
-        //   u => u.email === credentials.email && u.password == credentials.password
-        // );
 
-        // return (user ? user : null);
+        const users = [
+          { id: "1001", email: "admin1@example.com", password: "admin123" },
+          { id: "1002", email: "user1@example.com", password: "user123" }
+        ];
+        const user = users.find(
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          u => u.email === credentials.email && u.password == credentials.password
+        );
+
+        return (user ? user : null);
         // http://localhost:3000/studio/
         // $2a$12$S.EfYuHnJcJ0Z1oVPMxIoefFsJ0xFSZtj0Z4SdKgIbm1ap39.nfJO
         // a1b2c3d4e5f6g7h8i9j0
-
-        //         import { hash } from 'bcrypt-ts';
-        //
-        //         async function hashPassword(plainPassword) {
-        //           const hashedPassword = await hash(plainPassword, 10);  // Salt rounds = 10
-        //           console.log(hashedPassword);  // Use this hash when creating user records
-        //           return hashedPassword;
-        //         }
-        //
-        // // Example usage
-        //         const password = "admin123";
-        //         hashPassword(password);
 
       }
     })
